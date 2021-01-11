aadc283092ebca2f84bca7ea153374f262cfc793
J. Cool <jessescool@gmail.com>
2021-01-11 14:39:09 -0800

    Favicon to .svg, publi.sh completed

--- creation/publi.sh
+++ creation/publi.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # A script for pushing code changes AND new questions to web server
-# Takes a relative pathname ../
 
-# removing...
-echo "removing"
+# updating startrekdata.csv
+ipy desposit.py
 
-# replacing...
-# scp -r ../* $MEME9/TrekTrivia
+# syncing with server (not clear whether -del would work, too)
+rsync -rv --delete-before ../ $MEME9/TrekTrivia
 
-# maybe just normal scp will work... but need overwrite.
-# try RSYNC
-
-rsync -av --delete ../ $MEME9/TrekTrivia
-# I'm unsure about the slashes after the directory names, as well as the need for -a, -v, and whether meme9 has rsync installed.
+	# rsync versions are different...
+	# maybe do -del?
+	# would -a do something useful?
