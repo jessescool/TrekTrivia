f84370fba79b0e88488813615c056b648fc83756
J. Cool <jessescool@gmail.com>
2021-01-12 18:37:14 -0800

    More questions

--- creation/publi.sh
+++ creation/publi.sh
@@ -3,11 +3,23 @@
 # A script for pushing code changes AND new questions to web server
 
 # updating startrekdata.csv
-ipy desposit.py
+# ipy desposit.py
 
 # syncing with server (not clear whether -del would work, too)
-rsync -rv --delete-before ../ $MEME9/TrekTrivia/
+# rsync -rv --delete-before ../ $MEME9/TrekTrivia/
 
 	# rsync versions are different...
 	# maybe do -del?
 	# would -a do something useful?
+
+#---- an archaic version ----
+
+# this is not dynamic whatsoever
+# must be run from inside creation
+# doesn't create new vault...
+
+ssh admin@summitdesktops.com 'cd /var/www/meme9.com/TrekTrivia ; rm -rf *'
+echo "---deleted files on server---"
+
+scp -r ../* admin@summitdesktops.com:/var/www/meme9.com/TrekTrivia
+echo "---copied new files to server---"
--- creation/sket.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# this is not dynamic whatsoever
-
-ssh admin@summitdesktops.com 'cd /var/www/meme9.com/TrekTrivia ; rm -rf *'
-echo "deleted files on server"
-
-scp -r ../* admin@summitdesktops.com:/var/www/meme9.com/TrekTrivia
-echo "copied new files to server"
