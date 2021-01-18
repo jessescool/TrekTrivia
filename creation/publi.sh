bd0161557243ceb3fcf0ec86d12657179ea1d32c
J. Cool <jessescool@gmail.com>
2021-01-18 10:44:45 -0800

    Changed meta

--- creation/publi.sh
+++ creation/publi.sh
@@ -21,5 +21,5 @@
 ssh admin@summitdesktops.com 'cd /var/www/meme9.com/TrekTrivia ; rm -rf *'
 echo "---deleted files on server---"
 
-scp -r ../* admin@summitdesktops.com:/var/www/meme9.com/TrekTrivia
+scp -r /Users/jessecool/Library/Mobile\ Documents/com~apple~CloudDocs/Projects/TrekTrivia/* admin@summitdesktops.com:/var/www/meme9.com/TrekTrivia
 echo "---copied new files to server---"
